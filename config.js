d728815bb8f68991faa0c7a374c4a71e88fcb5a5
Abhishek Balaji <abhishek.balaji2@gmail.com>
2019-03-20 08:26:38 +0530

    Updated to Kiosk only
    Fixed Hasjob title
    Added flag to make it kiosk only

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.config = {
    "model_": "AppConfig",
    "id": 1,
-   "appName": "HasJob Kiosk [Fragments]",
+   "appName": "Hasjob Kiosk [Fragments]",
    "homepage": "https://fragments.hasjob.co/admin/kiosk?enable=1",
    "enableLogoutBttn": false,
    "rotation": "90",
