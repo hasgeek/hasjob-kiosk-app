ca0cc574c11066b09280a8f6fb69675a5a94687c
Abhishek Balaji <abhishek.balaji2@gmail.com>
2019-03-20 08:18:25 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,9 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "HasJob Kiosk [Fragments]",
+   "homepage": "https://fragments.hasjob.co/admin/kiosk?enable=1",
+   "enableLogoutBttn": false,
+   "rotation": "90",
+   "kioskEnabled": true
+};
