d6e33335de3e6ed22e6900efa096610295c6d493
Abhishek Balaji <abhishek.balaji2@gmail.com>
2019-06-11 15:23:00 +0530

    Updated for Rootconf

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 window.config = {
    "model_": "AppConfig",
    "id": 1,
-   "appName": "Hasjob Kiosk [Fragments]",
-   "homepage": "https://fragments.hasjob.co/admin/kiosk?enable=1",
+   "appName": "Hasjob Kiosk - Rootconf",
+   "homepage": "https://rootconf.hasjob.co/admin/kiosk?enable=1",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": true,
    "enableLogoutBttn": false,
+   "sessionDataTimeoutTime": 2,
+   "sessionTimeoutTime": 2,
    "rotation": "90",
    "kioskEnabled": true
 };
